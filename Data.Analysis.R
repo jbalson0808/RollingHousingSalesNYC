13ed1c6fc15c136f05f6ba3995fe38739aefcd61
jbalson0808 <balsonj0808@yahoo.com>
2017-06-15 21:35:51 -0500

    Add files via upload

---
+++ Data.Analysis.R
@@ -0,0 +1,9 @@
+
+###############
+#Data.Analysis
+###############
+
+plot(Manhattan.sale$gross.sqft,Manhattan.sale$sale.price.n)
+plot(log10(Manhattan.sale$gross.sqft),log10(Manhattan.sale$sale.price.n))
+plot(log10(Manhattan.homes$gross.sqft),log10(Manhattan.homes$sale.price.n))
+plot(log10(Manhattan.homes$gross.sqft),log10(Manhattan.homes$sale.price.n))
